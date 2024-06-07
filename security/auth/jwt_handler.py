b13654ef110c7b9066db1461ea70f104cfcd3d59
Marina Pintado <darkhuoyt@gmail.com>
2024-06-07 10:55:55 +0200

    jwt_handler

--- security/auth/jwt_handler.py
+++ security/auth/jwt_handler.py
@@ -16,3 +16,14 @@ def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+def verify_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        return payload
+    except JWTError:
+        raise credentials_exception
